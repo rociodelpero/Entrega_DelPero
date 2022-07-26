5520bb49749f65fc40f162bdd8e6f47817e49875
rociodelpero <rociodelpero1@gmail.com>
2022-07-26 17:28:38 -0300

    agregado de comentarios

--- app.js
+++ app.js
@@ -1,66 +1,14 @@
 
 
-//   un while que dentro tenga llamadas a funciones
-//  entonces arman un sistema donde el usuario entra y sale cuando quiere
-//  piedra papel o tijera
+//CALCULADORA INTERACTIVA
 
-// let respuestaUsuario = confirm ('¿Contamos hasta 100?')
-
-// while (respuestaUsuario) {
-//     alert ('Entramos en el bucle')
-//     respuestaUsuario = confirm ('Querés seguir?')
-// }
-
-// function redondeo(valor){
-//     return Math.round(valor)
-// }
-// for (let index = 0; index < 5; index++) {
-//     let entrada = prompt("INGRESAR NUMERO");
-//     alert(redondeo(entrada));
-// }
-
-
-// function validacion(cadena){
-//     return cadena != '';
-// }
-// let entrada =  prompt("INGRESAR CADENA");
-// while (entrada != 'ESC') {
-//     alert(validacion(entrada));
-//     entrada = prompt("INGRESAR CADENA");
-// }
-
-
-// function numeroMaximo (valor1, valor2) {
-//     if ( valor1 > valor2 ) {
-//         return valor1;
-//         }
-//         return valor2;
-// }
-
-// numeroMaximo (10,20)
-// console.log(numeroMaximo)
-
-
-// function saludar() {
-//     respuesta = prompt ("Hola, ¿cómo estuvo tu día?");
-//     if (respuesta === "bien") {
-//         alert("me alegro")
-//     } else {
-//         alert("una pena")
-//     }  
-//     }
-    
-// saludar()
-
+//Creación del while, el bucle sigue hasta que el usuario tipea ESC
 let respuestaUsuario = prompt ("¿Querés entrar a la calculadora?")
 
 while (respuestaUsuario != "ESC") {
     alert ('Entramos en la calculadora')
-    //respuestaUsuario = prompt ("Querés seguir?")
- //}
 
-//calculadora
-//Creación funciones de cálculo
+//Creación funciones de cálculo (flecha)
 const sumar = ( num1, num2 ) => {
     return parseInt (num1) + parseInt (num2);
 }
@@ -81,7 +29,7 @@ alert ("¿Qué cálculo querés hacer?")
 let calculo = prompt("1: suma,  2: resta,  3: multiplicación,  4: división")
 
 if ( calculo == 1 ) {
-    let numero1 = prompt("igresá el primer dígito para sumar");
+    let numero1 = prompt("ingresá el primer dígito para sumar");
     let numero2 = prompt("ingresá el segundo número para sumar");
     resultado = sumar (numero1,numero2);
     alert(`tu resultado es ${resultado}`)
@@ -89,27 +37,31 @@ if ( calculo == 1 ) {
 }
 
 else if ( calculo == 2 ) {
-    let numero1 = prompt("igresá el primer dígito para restar");
+    let numero1 = prompt("ingresá el primer dígito para restar");
     let numero2 = prompt("ingresá el segundo número para restar");
     resultado = restar (numero1,numero2);
     alert(`tu resultado es ${resultado}`)
     respuestaUsuario = prompt ("Querés seguir?")
 }
+
 else if ( calculo == 3 ) {
-    let numero1 = prompt("igresá el primer dígito para multiplicar");
+    let numero1 = prompt("ingresá el primer dígito para multiplicar");
     let numero2 = prompt("ingresá el segundo número para multiplicar");
     resultado = multiplicar (numero1,numero2);
     alert(`tu resultado es ${resultado}`)
     respuestaUsuario = prompt ("Querés seguir?")
 }
+
 else if ( calculo == 4 ) {
-    let numero1 = prompt("igresá el primer dígito para dividir");
+    let numero1 = prompt("ingresá el primer dígito para dividir");
     let numero2 = prompt("ingresá el segundo número para dividir");
     resultado = dividir (numero1,numero2);
     alert(`tu resultado es ${resultado}`)
     respuestaUsuario = prompt ("Querés seguir?")
 }
+
 else {
     alert ("error, no se encontró la operación")
 }
+
 }
